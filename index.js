c9d3560942ab34c2acbb87ceff0cbade79894a11
Birattam Rajak <birattam444@gmail.com>
2023-03-26 23:11:13 +0530

    adding js

---
+++ index.js
@@ -0,0 +1,34 @@
+const nextEl = document.querySelector(".next");
+const prevEl = document.querySelector(".prev");
+
+const imageContainerEl = document.querySelector(".image-container");
+
+const imgsEl = document.querySelectorAll("img");
+
+let currentImg = 1;
+
+nextEl.addEventListener("click", () => {
+  currentImg++;
+  updateImg();
+});
+
+prevEl.addEventListener("click", () => {
+  currentImg--;
+  updateImg();
+});
+
+updateImg();
+
+function updateImg() {
+  if (currentImg > imgsEl.length) {
+    currentImg = 1;
+  } else if (currentImg < 1) {
+    currentImg = imgsEl.length;
+  }
+  imageContainerEl.style.transform = `translateX(-${(currentImg - 1) * 500}px)`;
+
+  setTimeout(() => {
+    currentImg++;
+    updateImg();
+  }, 3000);
+}
